c1ac28570adc391426d0e7548084caa6637664f5
urquiagal <urquiagal@law.byu.edu>
2018-10-26 15:53:05 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,25 @@
+//set count
+if(localStorage.getItem("count") == null) {
+	localStorage.setItem("count", 0);
+}
+
+//update count in html
+function update() {
+	document.getElementById("count").innerHTML = localStorage.getItem("count");
+}
+
+update();
+
+//add one to count
+function plusOne() {
+	localStorage.setItem("count", (Number(localStorage.getItem("count")) + 1));
+	update();
+}
+
+//reset count
+function reset() {
+	if (confirm("Are you sure you want to reset?")) {
+    	localStorage.setItem("count",0);
+		update();
+	}
+}
